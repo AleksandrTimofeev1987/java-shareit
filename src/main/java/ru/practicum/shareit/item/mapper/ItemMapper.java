4a82c129c4132001b8066bfe06bcf8c0054c4783
Aleksandr Timofeev <timofeev.malakut@gmail.com>
2022-12-02 09:10:11 +0300

    ShareIt, Sprint 13 refactor: introduce mapstruct instead of custom mapper for User and Item

--- src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -1,8 +1,10 @@
 package ru.practicum.shareit.item.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import ru.practicum.shareit.item.dto.ItemCreateRequest;
 import ru.practicum.shareit.item.dto.ItemResponse;
+import ru.practicum.shareit.item.dto.UpdateItemDto;
 import ru.practicum.shareit.item.model.Item;
 
 @Mapper(componentModel = "spring")
@@ -11,4 +13,7 @@ public interface ItemMapper {
     ItemResponse toItemResponse(Item item);
 
     Item toItem(ItemCreateRequest itemDto);
+
+    @Mapping(target = "id", ignore = false)
+    Item toItem(UpdateItemDto itemDto);
 }
