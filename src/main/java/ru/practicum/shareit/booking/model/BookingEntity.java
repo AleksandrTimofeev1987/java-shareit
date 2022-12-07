ced5dca94001d99b6b9e8d661b8bfd63e4b86241
Aleksandr Timofeev <timofeev.malakut@gmail.com>
2022-12-08 02:01:01 +0300

    ShareIt, Sprint 14 Fully functional application under requirements
    fix: fix checks for operand being a Booking in BookingEntity
    
    NOTE: passing all users, items, bookings and comments postman-tests

--- src/main/java/ru/practicum/shareit/booking/model/BookingEntity.java
+++ src/main/java/ru/practicum/shareit/booking/model/BookingEntity.java
@@ -37,8 +37,8 @@ public class BookingEntity {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Booking)) return false;
-        return id != null && id.equals(((Booking) o).getId());
+        if (!(o instanceof BookingEntity)) return false;
+        return id != null && id.equals(((BookingEntity) o).getId());
     }
 
     @Override
